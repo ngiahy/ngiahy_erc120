98b718bfd9d5b912ba63097e4ade51497eeeb3c3
ngiahy <hya4csltt@gmail.com>
2024-09-08 10:47:01 +0700

    one commit

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+};
+require("@nomicfoundation/hardhat-toolbox");
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.20",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+};
